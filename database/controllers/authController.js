d39d1152ae92d3dfaacd7fe203f3281d49b5e8da
futurity <yoshimusibass@gmail.com>
2022-07-14 12:49:33 -0300

    add jsonwebtoken to auth

--- database/controllers/authController.js
+++ database/controllers/authController.js
@@ -1,40 +1,54 @@
 const express = require('express');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const authConfig = require('../config/auth');
 
 const Usuario = require('../models/Usuario');
 const router = express.Router();
 
-router.post('/register', async (req,res) => {
+const generateToken = (params = {}) => {
+    const token = jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400
+    })
+    return token
+}
+
+router.post('/register', async (req, res) => {
     const { email, username } = req.body;
     try {
-        if(await Usuario.findOne({ email }))
+        if (await Usuario.findOne({ email }))
             return res.status(400).json({ error: "Email is already registered" });
-        if(await Usuario.findOne({ username }))
+        if (await Usuario.findOne({ username }))
             return res.status(400).json({ error: "Username is already registered" });
-        const usuario = await Usuario.create(req.body);
-        return res.status(200).json({usuario});
-    } catch(err) {
+        const user = await Usuario.create(req.body);
+        return res.status(200).json({ user, token: generateToken({ id: user._id }) });
+    } catch (err) {
         console.log(err)
-        return res.status(400).json({error: 'erro ao registrar'});
+        return res.status(400).json({ error: 'erro ao registrar' });
     }
 })
 
-router.post('/authenticate', async (req,res) => {
+router.post('/authenticate', async (req, res) => {
     const { email, password } = req.body;
-    if(!password) return res.status(400).json({ error: "Password is required"})
+    if (!password) return res.status(400).json({ error: "Password is required" })
     try {
         const user = await Usuario.findOne({ email }).select('+password');
-        if(!user) 
+        if (!user)
             return res.status(400).json({ error: "user not found" });
-        
-        if(!await bcrypt.compare(password, user.password))
+
+        if (!await bcrypt.compare(password, user.password))
             return res.status(400).json({ error: "Invalid password" })
         user.password = undefined
-        return res.status(200).json({ user })
-         
-    } catch(err) {
+
+        return res.status(200).json({
+            user,
+            token: generateToken({ id: user._id })
+        })
+
+    } catch (err) {
         console.log(err)
-        return res.status(400).json({error: 'erro ao fazer login'});
+        return res.status(400).json({ error: 'erro ao fazer login' });
     }
 })
 
